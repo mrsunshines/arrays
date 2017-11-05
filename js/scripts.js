8380c3afc76d006941c7b6c7ceb473e1bc87a52f
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-05 18:59:29 +0100

    First array

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+﻿// arrays  operations
+var masc = ['Adam', 'Robert', 'Wergilius', 'Bercik', 'Duposław'],
+	femn = ['Adela', 'Regina', 'Weronika', 'Beata', 'Cycelia'],
+	mascAndFemn = masc.concat(femn),
+	newName = prompt ('Check name to to added to array'); 
+if (mascAndFemn.indexOf(newName) === -1) { 
+	alert(mascAndFemn.push(newName));
+} else {
+	alert('Try again - we know this name already - press OK, Refresh (F5) and try other name');
+}
